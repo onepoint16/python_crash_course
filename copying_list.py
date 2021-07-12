2eb80a1f3be6ebf699623627f7ef62dd2d7047a3
Sam <43417109+onepoint16@users.noreply.github.com>
2021-07-12 19:27:44 +0100

    first commit

---
+++ copying_list.py
@@ -0,0 +1,18 @@
+my_pizzas = ['classic', 'pepperoni', 'meat feast']
+
+kathryns_pizzas = my_pizzas[:]
+
+print(my_pizzas)
+
+print(kathryns_pizzas)
+
+kathryns_pizzas.append('grandma style')
+
+print(my_pizzas)
+
+print(kathryns_pizzas)
+
+kathryns_pizzas.pop(2)
+
+print(kathryns_pizzas)
+
